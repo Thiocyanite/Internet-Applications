61d5faad70c8947610bf82509f702adf285405b4
Julia <azotowyv@gmail.com>
2020-03-31 12:23:47 +0200

    Do 22

---
+++ zadanie3/form_check.js
@@ -0,0 +1,95 @@
+
+function validate(formularz) {
+  checkAndFocus(formularz.elements["f_imie"].value,"imie!",isWhiteSpaceOrEmpty);
+  checkString(formularz.elements["f_nazwisko"].value,"nazwisko!", isWhiteSpaceOrEmpty);
+  checkString(formularz.elements["f_kod"].value, "kod!", isWhiteSpaceOrEmpty);
+  checkString(formularz.elements["f_ulica"].value),"ulica!", isWhiteSpaceOrEmpty);
+  checkString(formularz.elements["f_miasto"].value,"miasto!",isWhiteSpaceOrEmpty);
+  checkEmail(formularz.elements["f_email"].value,"mejl!",isEmailInvalid);
+}
+
+function checkString(what, comment){
+  if (isEmpty(what) || isWhiteSpaceOrEmpty(what))
+    {
+      alert("Podaj "+comment+"!");
+      return false;
+    }
+  return true;
+}
+function isEmpty(what) {
+  if (what.lenght==0)
+    return true;
+  return false;
+}
+
+function isWhiteSpaceOrEmpty(str){
+  return /^[\s\t\r\n]*$/.test(str);
+}
+
+function checkEmail(str){
+  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+  if(email.test(str))
+    return true;
+  else
+  {
+    return false;
+  }
+}
+
+
+function isEmailInvalid(str){
+  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+  if(email.test(str))
+    return false;
+  else
+  {
+    return true;
+  }
+}
+
+function checkAndFocus(obj, msg, fun) {
+  let str = obj.value;
+  let errorFieldName = "e_"+ obj.name.substr(2, obj.name.length);
+  if (fun(obj))
+  {
+    document.getElementById(errorFieldName).innerHTML = msg;
+    obj.focus();
+    return false;
+  }
+  else{
+    document.getElementById(errorFieldName).innerHTML = "";
+    return true;
+  }
+}
+
+function checkStringAndFocus(obj, msg){
+  let str = obj.value;
+  let errorFieldName = "e_"+ obj.name.substr(2, obj.name.length);
+  if(isWhiteSpaceOrEmpty(str))
+  {
+    document.getElementById(errorFieldName).innerHTML = msg;
+    obj.focus();
+    return false;
+  }
+  else
+  {
+    document.getElementById(errorFieldName).innerHTML = "";
+    return true;
+  }
+}
+
+function checkEmailAndFocus(obj, msg){
+  let str = obj.value;
+  let errorFieldName = "e_"+ obj.name.substr(2, obj.name.length);
+  if(checkEmail(str))
+  {
+    document.getElementById(errorFieldName).innerHTML = msg;
+    obj.focus();
+    return false;
+  }
+  else
+  {
+    document.getElementById(errorFieldName).innerHTML = "";
+    return true;
+  }
+}
