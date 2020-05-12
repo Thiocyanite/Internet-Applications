1a9b27cc874c5fb9d0655559e1af200d4469a4ba
admin <admin@MacBook-Pro-admin.local>
2020-05-12 11:43:22 +0200

    Początek zadania 9

---
+++ zadanie9/zadanie9/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using zadanie9.Models;
+
+namespace zadanie9.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
